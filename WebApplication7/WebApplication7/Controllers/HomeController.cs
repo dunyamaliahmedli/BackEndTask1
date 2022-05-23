ac4b64a8882c3bf04d223671ee444703b559b8f7
dunyamaliahmedli <tu7bg7w6d@code.edu.az>
2022-05-23 09:58:54 +0300

    added task file

---
+++ WebApplication7/WebApplication7/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication7.DAL;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get;}
+
+        public HomeController(AppDbContext context)
+        {
+            _context=context;
+        }
+        public IActionResult Index()
+        {
+            List<Slider> sliders = _context.Sliders.ToList();
+
+            return View(sliders);
+        }
+    }
+}
